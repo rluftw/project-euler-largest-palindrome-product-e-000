c1662c4ad05774c2ea5585a23eb8d40223bee984
rluftw <richuuuurd@gmail.com>
2015-12-18 16:29:08 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+
+def is_palindrome(i)
+	i_as_string = i.to_s
+	i_as_string == i_as_string.reverse
+end
+
+def largest_palindrome_product
+	largest = 0
+	100.upto(999).select do |op1|
+		100.upto(999).select do |op2|
+			answer = op1 * op2
+			if is_palindrome(answer) && answer > largest
+				largest = answer
+			end
+		end
+	end
+	largest
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+	def is_palindrome(i)
+		i_as_string = i.to_s
+		i_as_string == i_as_string.reverse
+	end
+
+	def answer
+		largest = 0
+		100.upto(999).select do |op1|
+			100.upto(999).select do |op2|
+				answer = op1 * op2
+				if is_palindrome(answer) && answer > largest
+					largest = answer
+				end
+			end
+		end
+		largest
+	end
+end
